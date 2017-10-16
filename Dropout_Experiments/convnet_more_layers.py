f8cd2182e7cfb7b686d7bc483afd6b9c3e2a8073
Saad Abdullah Gondal <sag_21jan@hotmail.com>
2017-10-16 16:54:58 +0200

    Bug fixes

--- Dropout_Experiments/convnet_more_layers.py
+++ Dropout_Experiments/convnet_more_layers.py
@@ -103,14 +103,15 @@ def train():
         return {x: xs, y_: ys, global_step: i, keep_prob: k, }
 
     # weights initialization
-    def weight_variable(shape, stddev,name="W"):
-        initial = tf.truncated_normal(shape, stddev=stddev)
-        return tf.Variable(initial,name=name)
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
 
     # biases initialization
-    def bias_variable(shape,name="b"):
-        initial = tf.zeros(shape)
-        return tf.Variable(initial,name=name)
+    def bias_variable(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
     # define a 2d convolutional layer
     def conv_layer(input, channels_in, channels_out, name='conv'):
         with tf.name_scope(name):
