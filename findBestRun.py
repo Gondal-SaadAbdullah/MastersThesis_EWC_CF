13261bd6b436e6505b69a202cafa3d9e29dd6ba1
Saad Abdullah Gondal <sag_21jan@hotmail.com>
2017-11-03 09:45:35 +0100

    Error in EWC fixed. Path error in plotOneExp.py is fixed.

--- findBestRun.py
+++ findBestRun.py
@@ -5,17 +5,20 @@
 from __future__ import division
 from plotOneExp import readResults
 import sys, os
-import re, numpy as np,math
+import re, numpy as np, math
 
+from plotOneExp import readResults
+import sys, os, numpy as np
 
-from plotOneExp import readResults ;
-import sys,os,numpy as np ;
-w1 = 0.5 ; w2 = 0.5 ;
+w1 = 0.5
+w2 = 0.5
+
 
 # fitness function
 # takes a list of 3 np arrays, containing the accuracies over time from D1D1,D2D2, D2D1 (in that order)
 def measureQuality(D):
     return (D[0][:, 1]).max()  # criterion: highest initial training accuracy
+
 
 def measureQualityWithAvg(D, task):
     temp_acc = 0
@@ -27,46 +30,49 @@ def measureQualityWithAvg(D, task):
         else:
             temp_acc = acc_D2
 
+
 def measureQualityWithPcnt(D, task):
-    maxVal = D[1][:,1].max()
+    maxVal = D[1][:, 1].max()
     for idx, acc_D2 in enumerate(D[1][:, 1]):
         if acc_D2 >= maxVal * 0.98:
             D1_weight, D2_weight = getWeightsForAvg(task)
             acc_D1 = D[2][idx, 1]
             return np.average([acc_D1, acc_D2], weights=[D1_weight, D2_weight])
 
+
 def measureQualityAlex(D):
-  D2D2 = D[1] ;
-  maxD2D2 = D2D2[:,1].max() * 0.99;
-  for i in xrange(0,D2D2.shape[0]):
-    if D2D2[i,1] >= maxD2D2:
-      return w1 * D[2][i,1] + w2 * D[1][i,1];
+    D2D2 = D[1]
+    maxD2D2 = D2D2[:, 1].max() * 0.95
+    for i in xrange(0, D2D2.shape[0]):
+        if D2D2[i, 1] >= maxD2D2:
+            return w1 * D[2][i, 1] + w2 * D[1][i, 1]
+
 
 def measureQualityAlexD2D_1(D):
-  D2D2 = D[1] ;
-  maxD2D2 = D2D2[:,1].max() * 0.99;
-  for i in xrange(0,D2D2.shape[0]):
-    if D2D2[i,1] >= maxD2D2:
-      return w1 * D[3][i,1] + w2 * D[1][i,1];
-
+    D2D2 = D[1]
+    maxD2D2 = D2D2[:, 1].max() * 0.99
+    for i in xrange(0, D2D2.shape[0]):
+        if D2D2[i, 1] >= maxD2D2:
+            return w1 * D[3][i, 1] + w2 * D[1][i, 1]
 
 
 def extractTask(runID):
     return runID.split("_")[1]
 
+
 def getWeightsForAvg(task):
     p1 = int(re.search(r'\d+', task.split("-")[0]).group())
     p2 = int(re.search(r'\d+', task.split("-")[1]).group())
-    return p1/10, p2/10
+    return p1 / 10, p2 / 10
 
 
-expID = sys.argv[1] ;
+expID = sys.argv[1]
 pathString = "./"
 if len(sys.argv) > 2:
-  pathString = sys.argv[2]
-useMRL = False;
+    pathString = sys.argv[2]
+useMRL = False
 if len(sys.argv) > 3:
-  useMRL = True;
+    useMRL = True
 
 csvfiles = [f for f in os.listdir(pathString) if (f.find(".csv") != -1 and (f.split("_"))[0] == expID)]
 
@@ -88,45 +94,50 @@ for f in csvfiles:
 
 tasks = {}
 for key in expDict:
-  tasks[extractTask(key)] = True;
+    tasks[extractTask(key)] = True
 
+bestRunID = {key: None for key in tasks}
+bestFitness = {key: -1 for key in tasks}
+sumX = {key: 0. for key in tasks}
+sumX2 = {key: 0. for key in tasks}
+count = {key: 0. for key in tasks}
+for key, value in expDict.iteritems():
+    if len(value.keys()) >= 3:
+        print "valid exp", key
+        if key.find("9-1") != -1:
+            w1 = 0.9
+            w2 = 0.1
+        if key.find("5-5") != -1:
+            w1 = 0.5
+            w2 = 0.5
+        if key.find("10-10") != -1:
+            w1 = 0.5
+            w2 = 0.5
 
+        fitness = measureQualityAlex(readResults(key, pathString))
+        if useMRL == True:
+            fitness = measureQualityAlexD2D_1(readResults(key, pathString))
 
-bestRunID={key:None for key in tasks};
-bestFitness={key:-1 for key in tasks} ;
-sumX={key:0. for key in tasks} ;
-sumX2={key:0. for key in tasks} ;
-count={key:0. for key in tasks} ;
-for key,value in expDict.iteritems():
-  if len(value.keys()) >= 3:
-    print "valid exp", key ;
-    if key.find("9-1")!= -1:
-      w1 = 0.9 ; w2 = 0.1 ;
-    if key.find("5-5")!= -1:
-      w1 = 0.5 ; w2 = 0.5 ;
-    if key.find("10-10")!= -1:
-      w1 = 0.5 ; w2 = 0.5 ;
+        task = extractTask(key)
+        sumX[task] += fitness
+        sumX2[task] += fitness * fitness
+        count[task] += 1.0
+        if fitness > bestFitness[task]:
+            bestFitness[task] = fitness
+            bestRunID[task] = key
+    else:
+        print "invalid exp", key, len(value.keys())
 
-    fitness = measureQualityAlex(readResults(key,pathString)) ;
-    if useMRL==True:
-      fitness = measureQualityAlexD2D_1(readResults(key,pathString)) ;
-  
-    task = extractTask(key) ;
-    sumX [task ]+= fitness ;
-    sumX2 [task] += fitness*fitness ;
-    count [task] += 1.0 ;
-    if fitness> bestFitness[task]:
-      bestFitness[task]=fitness ;
-      bestRunID[task] = key ;
-  else:
-    print "invalid exp", key, len(value.keys()) ;
-  
+print tasks
 
+invalid_tasks = []
 for key in tasks:
-  print "Task ", key, ": best run was", bestRunID[key], " with a fitness of ", bestFitness[key], "mean/var=",sumX[key]/count[key],math.sqrt((sumX2[key]/count[key]-(sumX[key]/count[key])**2.)) ;
+    if bestRunID[key] is not None:
+        print "Task ", key, ": best run was", bestRunID[key], " with a fitness of ", \
+            bestFitness[key], "mean/var=", sumX[key] / count[key], \
+            math.sqrt((sumX2[key] / count[key] - (sumX[key] / count[key]) ** 2.))
+    elif key not in invalid_tasks:
+        invalid_tasks.append(key)
 
-
-  
-
-
-
+if invalid_tasks:
+    print "Some invalid experiment results for %s were omitted" % invalid_tasks
